d2f2567562fea6dd00e645c24a4c119bbead17dd
cksingh0033 <cksingh0033@gmail.com>
2026-01-26 16:18:40 +0530

    Make dependency-check script update

--- security/dependency-check.sh
+++ security/dependency-check.sh
@@ -4,15 +4,14 @@ set -e
 DC_VERSION=8.4.0
 DC_DIR="dependency-check"
 
-# Download only if not present
 if [ ! -d "$DC_DIR" ]; then
   curl -L https://github.com/jeremylong/DependencyCheck/releases/download/v${DC_VERSION}/dependency-check-${DC_VERSION}-release.zip -o dc.zip
   unzip dc.zip
 fi
 
-# Run scan
 ./${DC_DIR}/bin/dependency-check.sh \
   --project "swiggy" \
   --scan . \
   --format HTML \
-  --out dependency-check-report
+  --out dependency-check-report \
+  --nvdApiKey "$NVD_API_KEY"

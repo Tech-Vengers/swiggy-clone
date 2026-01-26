ca3235eb6d862826608c3f7aa4394a419aa70591
cksingh0033 <cksingh0033@gmail.com>
2026-01-26 16:07:26 +0530

    dependency file commit

---
+++ security/dependency-check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+DC_VERSION=8.4.0
+DC_DIR="dependency-check"
+
+if [ ! -d "$DC_DIR" ]; then
+  curl -L https://github.com/jeremylong/DependencyCheck/releases/download/v${DC_VERSION}/dependency-check-${DC_VERSION}-release.zip -o dc.zip
+  unzip dc.zip
+  mv dependency-check ${DC_DIR}
+fi
+
+./${DC_DIR}/bin/dependency-check.sh \
+  --project "swiggy" \
+  --scan . \
+  --format HTML \
+  --out dependency-check-report

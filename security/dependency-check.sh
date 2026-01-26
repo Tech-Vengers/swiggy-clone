8f07b95d3fb8c47f7310ed9e6b84da2f802aa7c1
cksingh0033 <cksingh0033@gmail.com>
2026-01-26 16:14:22 +0530

    Make dependency-check script update

--- security/dependency-check.sh
+++ security/dependency-check.sh
@@ -4,12 +4,13 @@ set -e
 DC_VERSION=8.4.0
 DC_DIR="dependency-check"
 
+# Download only if not present
 if [ ! -d "$DC_DIR" ]; then
   curl -L https://github.com/jeremylong/DependencyCheck/releases/download/v${DC_VERSION}/dependency-check-${DC_VERSION}-release.zip -o dc.zip
   unzip dc.zip
-  mv dependency-check ${DC_DIR}
 fi
 
+# Run scan
 ./${DC_DIR}/bin/dependency-check.sh \
   --project "swiggy" \
   --scan . \
